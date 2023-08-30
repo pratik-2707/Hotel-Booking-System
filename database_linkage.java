c4ac05d88650bb96de62db7fc61474dc55da2b54
Pratik <73239498+pratik-2707@users.noreply.github.com>
2023-08-30 09:21:06 +0530

    Update database_linkage.java

--- database_linkage.java
+++ database_linkage.java
@@ -4,19 +4,20 @@ import java.sql.SQLException;
 
 public class database_linkage {
         static Connection con = connectionfactory.getconnection();
-        public static void insert(int id ,String name, int total_members, int phone, String checkin , int bill,String typeOfroom) throws SQLException {
+        public static void insert(int id ,String name, int total_members, String phone, String checkin , int bill,String typeOfroom) throws SQLException {
             final String SQL = "insert into miniproject.visitors values (?,?,?,?,?,?,?)" ;
             try (PreparedStatement stmt = con.prepareStatement(SQL)) {
                 stmt.setInt(1, id);
                 stmt.setString(2, name);
                 stmt.setInt(3, total_members);
-                stmt.setInt(4, phone);
+                stmt.setString(4, phone);
                 stmt.setString(5, checkin);
                 stmt.setInt(6, bill);
                 stmt.setString(7, typeOfroom);
                 stmt.executeUpdate();
             }
                 
-        }    
+        }
+
 }        
 
