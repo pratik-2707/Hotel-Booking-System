de57648a6a2332010da5b4134c39c5e6a4c736ea
Pratik <73239498+pratik-2707@users.noreply.github.com>
2023-08-30 09:21:47 +0530

    Update connectionfactory.java

--- connectionfactory.java
+++ connectionfactory.java
@@ -4,8 +4,8 @@ import java.util.Properties;
 
 public class connectionfactory {
     public static final String USERNAME = "root" ;
-    public static final String PASSWORD  =  "your_database_password";           // password
-    public static final String URL = "jdbc:mysql://localhost:3306/your_databasename_in_your_pc" ;     // database_name
+    public static final String PASSWORD  =  ""; // your password
+    public static final String URL = "jdbc:mysql://localhost:3306/miniproject" ;
     
     public static Connection getconnection() {
         Connection con = null ;
